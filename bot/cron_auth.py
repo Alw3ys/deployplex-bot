1b83bed578d8b216de040d0778410d1c5e24c67a
Alw3ys <alw3ys@gmail.com>
2023-10-30 20:49:08 +0100

    feat: reset

--- bot/cron_auth.py
+++ bot/cron_auth.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException, Request, status
 
-CRON_SECRET = "YOUR_SECRET_VALUE_HERE"
+DEPLOYPLEX_CRON_SECRET = "YOUR_SECRET_VALUE_HERE"
 
 
 def authenticate_request(request: Request):
     auth_header = request.headers.get("authorization")
-    if auth_header != f"Bearer {CRON_SECRET}":
+    if auth_header != f"Bearer {DEPLOYPLEX_CRON_SECRET}":
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
